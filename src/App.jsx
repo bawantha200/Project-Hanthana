7d0fde27f08a0887885e71c8295c9fe5430fb4c7
jani4429 <jmalavipathirana@gmail.com>
2026-06-13 13:12:26 +0530

    Employee & HRM

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Router from "./router/router.jsx";
-const App = () => {
-    return (
-        <Router/>
-    );
-};
-
-export default App;
--- src/components/RoleBadge.jsx
+++
@@ -1,9 +0,0 @@
-import { getRoleColor } from '../utils/helpers';
-
-export default function RoleBadge({ role }) {
-  return (
-    <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium border ${getRoleColor(role)}`}>
-      {role}
-    </span>
-  );
-}
